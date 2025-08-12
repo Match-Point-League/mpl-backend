5f5c3a6d8f1b67dace6161229c86f6a01bb85e3d
vrindasehgal <vrinda.s2811@gmail.com>
2025-08-12 17:07:19 +0530

    added utils for validation since a lot of them may be used in several features

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Utils index
+ * 
+ * This module exports all utility functions and constants
+ * for easy importing throughout the application.
+ */
+
+// Validation utilities
+export * from './validation';
